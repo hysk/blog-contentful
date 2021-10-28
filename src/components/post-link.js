8a7848d60f8a511fb2aae2e27e3e3709567b307e
hysk <hysk13@gmail.com>
2021-10-29 08:08:11 +0900

    conditional image

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import NO_IMAGE from "../../static/no_image_yoko.jpeg"
 import "../styles/post-link.css"
 
 export default function PostLink({ post }) {
@@ -7,16 +8,26 @@ export default function PostLink({ post }) {
   const description = post.body ? post.body.raw : 'no desc';
   const pageLink = `/post/${post.slug}/`
 
+  let image;
+  if (coverImage) {
+    image = coverImage.file.url;
+  } else {
+    image = NO_IMAGE
+  }
+
+  console.log(image)
+
+          //<!-- <img src={coverImage.file.url} className="post-link-image" alt="post-cover"></img> -->
   return (
     <Link to={pageLink} className="post-link-anchor">
       <div className="post-link">
-        <div>
-          <img src={coverImage.file.url} className="post-link-image" alt="post-cover"></img>
-        </div>
         <div clanName="post-link-text">
           <h2>{title}</h2>
           <p className="post-link-date">{publishDate}</p>
           <p className="post-link-body">{description}</p>
+        </div>
+        <div>
+          <img src={image} className="post-link-image" alt="post-cover"></img>
         </div>
       </div>
     </Link>
--- src/templates/post.js
+++ src/templates/post.js
@@ -7,13 +7,18 @@ export default function Post({ pageContext }) {
   //const body = pageContext.post.bodyMarkdown.bodyMarkdown.childMarkdownRemark.html;
   const body = pageContext.post.bodyMarkdown.childMarkdownRemark.html;
 
+  let cover;
+  if (coverImage) {
+    cover = <img src={coverImage.file.url} className="post-image" alt="post-cover"></img>;
+  }
+
   return (
     <Layout>
       <div className="post-header">
         <h1>{title}</h1>
         <p className="post-date">{publishDate}</p>
       </div>
-      <img src={coverImage.file.url} className="post-image" alt="post-cover"></img>
+      {cover}
       <div dangerouslySetInnerHTML={{ __html: body }} className="post-body" />
     </Layout>
   )
